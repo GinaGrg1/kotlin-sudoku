d6ec59e77f741b6ab4ead561c734d4e5f625acaf
Regina Gurung <reginagurung@yahoo.co.uk>
2018-03-07 21:29:19 +0000

    Add the main and test file

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by ReginaGurung on 27/02/2018.
+ */
+
+//fun main(args : Array<String>) {
+//    val name = "Alex" /** to reassign use var.*/
+//
+//    val firstNumber = 9
+//    val secondNumber = 7
+//
+//    println("This is my first Sudoku game!. My name is $name")
+//    println("$firstNumber * $secondNumber = ($firstNumber* $secondNumber)")
+//}
+
+fun main(args: Array<String>){
+    GameFrame(SudokuGame())
+}
+
---
+++ src/SudokuGameTest.kt
@@ -0,0 +1,16 @@
+/**
+ * Created by ReginaGurung on 06/03/2018.
+ */
+//
+//class SudokuGameTest{
+//
+//    val sudokuGame = SudokuGame()
+//
+//    @Test
+//    fun isUserInputCorrectFor3InRow0Column0(){
+//        val result:Boolean = sudokuGame.isUserInputCorrect(userInput:3, row:0, col:0)
+//
+//        assertTrue { result }
+//
+//    }
+//}
